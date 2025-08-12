0aed3072243703b892e4a5b876f79ee0cd20c9d8
Ana Carolina A Pereira <anacarolinaaguiarp@gmail.com>
2025-08-11 21:48:09 -0300

    add Aula04

---
+++ Aula04/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.box1}></View>
+      <View style={styles.box2}></View>
+      <View style={styles.box3}></View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, //Ocupa a Tela com todo o espaço disponível
+    backgroundColor: '#f2f2f2', //Cor de fundo do container
+    flexDirection: 'row', //Eixo principal
+    justifyContent: 'center', //Alinhamento do eixo principal
+    alignItems: 'center', //Alinhamento do eixo secundário
+  },
+
+  box1: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'red',
+    margin: 5
+  },
+
+  box2: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'green',
+    margin: 5
+  },
+  
+  box3: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'blue',
+    margin: 5
+  },
+});
---
+++ Aula04/src/pages/Principal.js
@@ -0,0 +1,34 @@
+import { StyleSheet, Text, View } from "react-native";
+
+
+export default () => (
+    <View style={Estilo.container}>
+        <View style={Estilo.box1} />
+        <View style={Estilo.box2} />
+        <View style={Estilo.box3} />
+    </View>
+);
+
+const Estilo = StyleSheet.create({
+    container: {
+        flex: 1, //Ocupa todo o espaço disponível
+        flexDirection: 'column',
+        backgroundColor: '#dadada',
+        justifyContent: 'center',
+    },
+
+    box1: {
+        backgroundColor: 'red',
+        flex: 1, //Ocupa 1 parte
+    },
+
+    box2: {
+        backgroundColor: 'green',
+        flex: 2, //Ocupa 2 partes
+    },
+
+    box3: {
+        backgroundColor: 'blue',
+        flex: 3, //Ocupa 3 partes
+    }
+});
