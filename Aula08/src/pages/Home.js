89dedd6b04e2d5ca47b7a9a5f89c28b0cd8f584b
Ana Carolina A Pereira <anacarolinaaguiarp@gmail.com>
2025-08-25 21:53:44 -0300

    add Aula08

---
+++ Aula08/src/pages/Home.js
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from "react-native"
+
+import Saudacao from "../components/Saudacao";
+
+export default function Home() {
+    return(
+        <View style={Estilo.container}>
+            <Text style={Estilo.titulo}>Pagina Inicial</Text>
+            <Saudacao nome="Helton Azevedo"/>
+        </View>
+    )
+}
+
+const Estilo = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    titulo:{
+        fontSize: 40
+    }
+})
