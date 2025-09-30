95c8d6ff80cc6a67a3de34e1b8fbda5c687c7df2
Ana Carolina A Pereira <anacarolinaaguiarp@gmail.com>
2025-09-29 21:47:28 -0300

    add Aula14

---
+++ Aula14/src/routes/DrawerRoutes.js
@@ -0,0 +1,21 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+import Home from "../pages/Home";
+import Pagamentos from "../pages/Pagamentos";
+import Pedidos from "../pages/Pedidos";
+
+const Drawer = createDrawerNavigator();
+
+export default () => (
+    <NavigationContainer>
+        <Drawer.Navigator
+            initialRouteName="Inicio"
+        >
+            <Drawer.Screen name="Inicio" component={Home} />
+            <Drawer.Screen name="Pagamento" component={Pagamentos} />
+            <Drawer.Screen name="Pedidos" component={Pedidos} />
+            
+        </Drawer.Navigator>
+    </NavigationContainer>
+)
